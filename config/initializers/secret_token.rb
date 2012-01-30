8b75a51ea05ce5eca1587252c264161517386ded
sriram ilapakurty <sriram.in20@gmail.com>
2012-01-30 22:22:15 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waterbukets::Application.config.secret_token = '165cd7453cfd7d84dd206db81da0b0ff5bab29679aef0a44c6dc9caa7a6d65af8eb908d35a43069a21bbbe11f912d713fde7092e57643409b30e0a7016651148'
